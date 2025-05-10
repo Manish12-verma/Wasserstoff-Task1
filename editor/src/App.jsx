7aa7ae95099e66ee8d9e0164e1065295c3096d97
Manish verma <110876979+Manish12-verma@users.noreply.github.com>
2025-05-10 17:21:26 +0530

    created editor

---
+++ editor/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import QuillEditor from './components/QuillEditor';
+
+function App() {
+  return (
+    <div >
+      <QuillEditor />
+    </div>
+  );
+}
+
+export default App;
